35215eb8983f041903430cce0b853e9486f0640d
haitang1894 <haitang1894@gmail.com>
2024-06-19 22:01:25 +0800

    🐛 fix: 修复数据库在新增、修改的时候获取不到人员信息问题

--- src/main/java/com/pea/common/mybatis/base/BaseEntity.java
+++ src/main/java/com/pea/common/mybatis/base/BaseEntity.java
@@ -3,7 +3,6 @@ package com.pea.common.mybatis.base;
 import cn.hutool.core.date.DatePattern;
 import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.TableField;
-import com.baomidou.mybatisplus.annotation.TableLogic;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
@@ -20,9 +19,11 @@ import java.time.LocalDateTime;
 public class BaseEntity implements Serializable {
 
 	@Schema(description = "创建者ID")
+	@TableField(fill = FieldFill.INSERT)
 	private Long createId;
 
 	@Schema(description = "创建者名称")
+	@TableField(fill = FieldFill.INSERT)
 	private String createBy;
 
 	@Schema(description = "创建时间")
@@ -31,14 +32,16 @@ public class BaseEntity implements Serializable {
 	private LocalDateTime createTime;
 
 	@Schema(description = "修改者ID")
+	@TableField(fill = FieldFill.UPDATE)
 	private Long updateId;
 
 	@Schema(description = "修改者名称")
+	@TableField(fill = FieldFill.UPDATE)
 	private String updateBy;
 
 	@Schema(description = "更新时间")
 	@JsonFormat(pattern = DatePattern.NORM_DATETIME_PATTERN, timezone = "GMT+8")
-	@TableField(fill = FieldFill.INSERT_UPDATE)
+	@TableField(fill = FieldFill.UPDATE)
 	private LocalDateTime updateTime;
 
 	@Schema(description = "是否已删除：0->未删除；1->已删除")
